211afc08e73954cb492bdd680b234033084b33d8
Chacha Nurholis <chachanurholis29@gmail.com>
2022-05-30 21:11:01 +0700

    feat: hello world & variables

---
+++ variables.dart
@@ -0,0 +1,14 @@
+var name = 'Chacha Nurholis';
+var year = 2002;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+void main() {
+  for (var i = 0; i < 2; i++) {
+    print(name);
+  }
+}
